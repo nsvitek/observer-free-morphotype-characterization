4df34ea315ec5f7c70fd05daa86c716fac2c8ac7
nsvitek <nsvitek@gmail.com>
2017-02-03 07:51:14 -0500

    bitty lines

--- temp_sim.R
+++ temp_sim.R
@@ -6,16 +6,18 @@ locateData<-"C:/Users/N.S/Dropbox/Documents/Dissertation/sensitivity_analysis/da
 setwd(locateScripts)
 source("sensitivity_dependencies.R")
 filename<-"morphologika_unscaled_high.txt"
+groups<-c(1,5,6,7,8,8,8,8,8,8,1,1,1,1,1,2,3,4) #set group ID's
+sim.palette<-brewer.pal(n=8,"PiYG") #set color palette
+
 setwd(locateData)
 setwd("simulation")
 lsdr<-list.dirs(path = ".", full.names = TRUE, recursive = FALSE)
-setwd(lsdr)
+setwd(lsdr[2])
 objs<-read.morphologika(filename)
 scld<-preprocess(objs)
 shps<-prcomp(scld$m2d,scale.=FALSE)
 dimnames(objs)[[3]]
-groups<-c(1,1,1,1,1,1,2,3,4,5,6,7,8,8,8,8,8) #set group ID's
-sim.palette<-brewer.pal(n=8,"PiYG") #set color palette
+cbind(dimnames(objs)[[3]],groups)
 
 plot(shps$x[,1:2],pch=21,bg=sim.palette[groups])
 dim(objs)
